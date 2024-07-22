63b45bab80ad1067e132215864db45cc64f07fd9
lazaro_suarez <desenvolvedor11@carrera.com.br>
2024-07-21 20:14:10 -0400

    modificado valor por defecto de campos de auditoria

--- API.Data/Entidades/EntitidadBase.cs
+++ API.Data/Entidades/EntitidadBase.cs
@@ -3,9 +3,9 @@
     public class EntidadBase
     {
         public Guid Id { get; set; }
-        public DateTime FechaCreado { get; set; } = DateTime.Now;
+        public DateTime FechaCreado { get; set; } = new DateTime();
         public string CreadoPor { get; set; } = string.Empty;
-        public DateTime FechaActualizado { get; set; } = DateTime.Now;
+        public DateTime FechaActualizado { get; set; } = new DateTime();
         public string ActualizadoPor { get; set; } = string.Empty;
     }
 }
